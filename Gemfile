168d81abd7cc5187668d1bda1f6bf7b32d5c0891
ILyass-Lr <lirmaqui.ilyass@gmail.com>
2025-04-27 11:57:11 +0100

    Finish working and refactored palindrome method.

--- Gemfile
+++ Gemfile
@@ -7,5 +7,5 @@ gemspec
 
 gem "irb"
 gem "rake", "~> 13.0"
-
+gem "minitest-reporters", "1.7.1"
 gem "minitest", "~> 5.16"
--- lib/ilyass_palindrome.rb
+++ lib/ilyass_palindrome.rb
@@ -2,7 +2,20 @@
 
 require_relative "ilyass_palindrome/version"
 
-module IlyassPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+  #  Returns true for a palinfrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse  
+  end
+
+  private
+
+    # Returns content for palindrome testing
+    def processed_content 
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
+# module IlyassPalindrome
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
